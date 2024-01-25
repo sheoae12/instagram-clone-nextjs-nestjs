a19bfb39efdd2318c1e47c705276ffaeadaafcb9
hslee <hslee@salin.co.kr>
2024-01-25 17:49:25 +0900

    [fix] add sample annotation

--- frontend/app/api/signin/route.ts
+++ frontend/app/api/signin/route.ts
@@ -6,6 +6,10 @@ export async function POST(req: Request) {
 	try {
 		const { account, password } = await req.json()
 
+		// 로컬 테스트
+        // const accessToken = signJwtAccessToken({ account });
+        // const result = { id: 1, account, accessToken };
+
 		const result = await axios.post(process.env.BACKEND_URL+'/auth/signin/common', {
 			account, password
 		})
