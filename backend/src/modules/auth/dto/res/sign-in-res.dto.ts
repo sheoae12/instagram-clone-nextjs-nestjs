a369467be61c60e23453c0c608ebbaf3dcf9743d
sheoae12 <sheoae@gmail.com>
2024-01-31 10:02:12 +0900

    [fix] add profileImg url to signin response

--- backend/src/modules/auth/dto/res/sign-in-res.dto.ts
+++ backend/src/modules/auth/dto/res/sign-in-res.dto.ts
@@ -5,11 +5,13 @@ export class SignInResponse {
     name: string;
     nickname: string;
     accessToken: string;
+    profileImg: string;
 
     constructor(data: any) {
         this.uid = data.userInfo.uid;
         this.name = data.userInfo.name;
         this.nickname = data.userInfo.nickname;
+        this.profileImg = data.userInfo.profileimg;
         this.accessToken = data.accessToken;
     }
 }
