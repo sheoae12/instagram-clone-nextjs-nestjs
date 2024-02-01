e75a4a4680c8fb1d42a35bd6ae89c9810dab31f4
sheoae12 <sheoae@gmail.com>
2024-02-01 17:28:47 +0900

    [feat] CommentInputField component (10%)

---
+++ frontend/components/field/CommentInputField.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import styles from './Field.module.css'
+import Image from "next/image"
+import Link from 'next/link'
+import React from "react"
+import { getTimeAgo } from '@/common/util/date'
+
+export default async function CommentInputField() {
+    return (
+        <></>
+    )
+}
